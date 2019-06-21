9af2b96e64870815ff8dc1bb6c92b2ccb8fb57fe
Anton <anton.backstrom@gmail.com>
2019-06-21 09:35:43 +0200

    Basically done with Lazy foo tutorial 3

--- main.c
+++ main.c
@@ -3,9 +3,29 @@
 #include <SDL.h>
 #include <stdbool.h>
 
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gImage = NULL;
+
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1600;
+const int SCREEN_HEIGHT = 900;
+
+char imagePath[24] = "Hello.bmp";
 
 //Main loop flag
 bool quit = false;
@@ -13,13 +33,165 @@ bool quit = false;
 //Event handler
 SDL_Event e;
 
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+    }
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    gImage = SDL_LoadBMP( imagePath );
+    if( gImage == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", imagePath, SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    //Deallocate surface
+    SDL_FreeSurface( gImage );
+    gImage= NULL;
+
+    //Destroy window
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
+
 int main( int argc, char* args[] )
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
+    //Start up SDL and create window
+    if( !init() )
+    {
+        printf( "Failed to initialize!\n" );
+    }
+    else
+    {
+        //Load media
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
+        }
+        else
+        {
+            //Apply the image
+            SDL_BlitSurface( gImage, NULL, gScreenSurface, NULL );
+            //Update the surface
+            SDL_UpdateWindowSurface( gWindow );
 
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+            //While application is running
+            while( !quit )
+            {
+                //Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                }
+            }
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Old
+
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 1600;
+const int SCREEN_HEIGHT = 900;
+
+//Main loop flag
+bool quit = false;
+
+//Event handler
+SDL_Event e;
+
+void createPic();
+
+//Picture to be placed in the window
+SDL_Surface* screenPicture = NULL;
+
+//The window we'll be rendering to
+SDL_Window* window = NULL;
+
+//The surface contained by the window
+SDL_Surface* screenSurface = NULL;
+
+int main( int argc, char* args[] )
+{
+
 
     //Initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
@@ -42,6 +214,7 @@ int main( int argc, char* args[] )
             //Fill the surface white
             SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
 
+            SDL_BlitSurface( screenPicture, NULL, screenSurface, NULL );
             //Update the surface
             SDL_UpdateWindowSurface( window );
 
@@ -70,3 +243,14 @@ int main( int argc, char* args[] )
     return 0;
 }
 
+void createPic()
+{
+    //Load splash image
+    screenPicture = SDL_LoadBMP( "Hello.bmp" );
+
+    if( screenPicture == NULL )
+    {
+    printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
+    }
+}
+*/
